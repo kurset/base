2f0b96e20a0ed67cff1d8f31a7a4dc4f71ad00ae
Kurset <haoyuace@163.com>
2018-02-03 16:30:04 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,38 @@
+var $ = function(selector) {
+    return $.fn.init(selector);
+}
+
+$.fn = $.prototype;
+
+$.fn.init = function(selector, context) {
+    // DOM element nodetype === 1
+    if (selector.nodeType === 1) {
+        this[0] = selector;
+        this.length = 1;
+        return this;
+    }
+
+    var parent = context || document;
+    var nodeList = parent.querySelectorAll(selector);
+
+    this.length = nodeList.length;
+
+    for (var i = 0; i < this.length; i += 1) {
+        this[i] = nodeList[i];
+    }
+    return this;
+}
+
+$.extend = $.fn.extend = function(destination, source) {
+
+    if (typeof source === 'undefined') {
+        source = destination;
+        destination = this;
+    }
+
+    for (var property in source) {
+        if (source.hasOwnProperty(property)) {
+            destination[property] = source[property];
+        }
+    }
+}
